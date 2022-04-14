4ba547e64911da81c381af2b91c59311dffc46be
RandeepSingh <s.randeep417@gmail.com>
2022-04-13 20:26:09 -0400

    First commit goggl

---
+++ src/contexts/StateContextProvider.js
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1'
+
+// https://google-search3.p.rapidapi.com/api/v1/search/q=elon+musk
+
+export const StateContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getResults = async (url) => {
+    setLoading(true);
+
+    const res = await fetch(`${baseUrl}${url}`, {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+        'X-RapidAPI-Key': '8d8a1e7778msh23af0ecfed5ce9fp1947f4jsna304592e1733'
+      },
+    });
+
+    const data = await res.json();
+    
+    console.log(data)
+
+    setResults(data);
+    setLoading(false);
+  };
+
+  return (
+    <StateContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
